a00f6423db261350bfbed6c8b061e4125be61487
Kostas Terzidis <imkostas@gmail.com>
2015-06-03 12:53:08 -0400

    added header file

---
+++ Better/Following.h
@@ -0,0 +1,13 @@
+//
+//  Following.h
+//  Better
+//
+//  Created by Kostas on 6/3/15.
+//  Copyright (c) 2015 Company. All rights reserved.
+//
+
+#ifndef Better_Following_h
+#define Better_Following_h
+
+
+#endif
