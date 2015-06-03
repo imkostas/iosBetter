a7db944d213b7a85f1bc3c424cd549e67111182c
Peter Goulakos <peteg944@gmail.com>
2015-06-03 12:54:34 -0400

    Added UserInfo class

---
+++ Better/UserInfo.h
@@ -0,0 +1,13 @@
+//
+//  UserInfo.h
+//  Better
+//
+//  Created by Peter on 6/3/15.
+//  Copyright (c) 2015 Company. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfo : NSObject
+
+@end
---
+++ Better/UserInfo.m
@@ -0,0 +1,15 @@
+//
+//  UserInfo.m
+//  Better
+//
+//  Created by Peter on 6/3/15.
+//  Copyright (c) 2015 Company. All rights reserved.
+//
+
+#import "UserInfo.h"
+
+@implementation UserInfo
+
+// Class for holding information
+
+@end
